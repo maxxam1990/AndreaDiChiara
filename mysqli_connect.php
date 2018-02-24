2ba6c6b50d9af1b6cb8af0cedb6b8d5a1d9c730f
Rahul Potluri <potlurir@kean.edu>
2018-02-24 16:10:46 -0500

    Change the DB Connection

--- mysqli_connect.php
+++ mysqli_connect.php
@@ -5,10 +5,10 @@
 // selects the database, and sets the encoding.
 
 // Set the database access information as constants:
-DEFINE ('DB_USER', 'id4827887_root');
+DEFINE ('DB_USER', 'epiz_21674585');
 DEFINE ('DB_PASSWORD', 'kean123');
-DEFINE ('DB_HOST', 'localhost');
-DEFINE ('DB_NAME', 'id4827887_seniorproject');
+DEFINE ('DB_HOST', 'sql201.epizy.com');
+DEFINE ('DB_NAME', 'epiz_21674585_Senior_Project');
 
 // Make the connection:
 $dbc = @mysqli_connect (DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) OR die ('Could not connect to MySQL: ' . mysqli_connect_error() );
